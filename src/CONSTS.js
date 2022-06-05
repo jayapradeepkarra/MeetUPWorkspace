a66f8001243843b90f3a58f7c5cf8cbee220ecfe
jayapradeepkarra <pradeepkarra29@gmail.com>
2022-06-05 10:51:46 +0530

    new id

--- src/CONSTS.js
+++ src/CONSTS.js
@@ -1,11 +1,11 @@
-// export const COMETCHAT_CONSTANTS = {
-//   APP_ID: 'APP_ID',
-//   REGION: 'REGION',
-//   AUTH_KEY: 'AUTH_KEY',
-// };
-
 export const COMETCHAT_CONSTANTS = {
-  APP_ID: '211063b7c958c9eb',
+  APP_ID: '2113564a3b91b310',
   REGION: 'us',
-  AUTH_KEY: '80103ea8320c55ccf9306cb31910ece5d59dcc8e',
+  AUTH_KEY: '6894ae637189f883f95dcc2e069a5f589df84e79',
 };
+
+// export const COMETCHAT_CONSTANTS = {
+//   APP_ID: '211063b7c958c9eb',
+//   REGION: 'us',
+//   AUTH_KEY: '80103ea8320c55ccf9306cb31910ece5d59dcc8e',
+// };
