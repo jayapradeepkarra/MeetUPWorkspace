3d5c0ae60c2d30879f56e4b9aef12a103cffc3c4
jayapradeepkarra <pradeepkarra29@gmail.com>
2022-06-05 09:18:49 +0530

    1st commit

---
+++ src/CONSTS.js
@@ -0,0 +1,11 @@
+// export const COMETCHAT_CONSTANTS = {
+//   APP_ID: 'APP_ID',
+//   REGION: 'REGION',
+//   AUTH_KEY: 'AUTH_KEY',
+// };
+
+export const COMETCHAT_CONSTANTS = {
+  APP_ID: '211063b7c958c9eb',
+  REGION: 'us',
+  AUTH_KEY: '80103ea8320c55ccf9306cb31910ece5d59dcc8e',
+};
---
+++ src/defaultPages/HomePage/index.js
@@ -0,0 +1,232 @@
+import React from 'react';
+import {connect} from 'react-redux';
+
+import styles from './styles';
+import * as actions from '../../store/action';
+import {Text, View, ScrollView, TouchableOpacity} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+
+const HomePage = (props) => {
+  if (!props.isLoggedIn) {
+    props.navigation.navigate('LoginPage');
+  }
+
+  return (
+    <ScrollView>
+      <SafeAreaView>
+        <View style={styles.wrapperStyle}>
+          <Text style={styles.titleStyle}>
+            WELCOME..TO..MEETUP..!
+          </Text>
+          <Text style={styles.subTitleStyle}>
+            SELECT LAUNCH OPTIONS..!
+          </Text>
+
+          <View style={styles.componentStyle}>
+            <View style={styles.boxStyle}>
+              <View style={styles.boxContent}>
+                <View style={styles.titleWrapperStyle}>
+                  <Text style={styles.componentTitleStyle}>MEETUPUI</Text>
+                </View>
+                <View style={styles.descWrapperStyle}>
+                  <Text>
+                    The <Text style={styles.descTextColor}>MEETUPUI</Text>{' '}
+                    component launches a fully working chat application.
+                  </Text>
+                </View>
+                <View style={styles.linkContainer}>
+                  <TouchableOpacity
+                    style={styles.linkWrapperStyle}
+                    onPress={() => {
+                      props.navigation.navigate('CometChatUI');
+                    }}>
+                    <Text style={styles.linkStyle}>Launch</Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
+            </View>
+          </View>
+
+          <View style={styles.componentStyle}>
+            <View style={styles.boxStyle}>
+              <View style={styles.boxContent}>
+                <View style={styles.titleWrapperStyle}>
+                  <Text style={styles.componentTitleStyle}>Conversations</Text>
+                </View>
+                <View style={styles.descWrapperStyle}>
+                  <Text>
+                    The{' '}
+                    <Text style={styles.descTextColor}>
+                      MeetupConversationListWithMessages
+                    </Text>{' '}
+                    component launches a Conversation list with messaging.
+                  </Text>
+                </View>
+                <View style={styles.linkContainer}>
+                  <TouchableOpacity
+                    style={styles.linkWrapperStyle}
+                    onPress={() => {
+                      props.navigation.navigate('Conversation');
+                    }}>
+                    <Text style={styles.linkStyle}>Launch</Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
+            </View>
+          </View>
+
+          <View style={styles.componentStyle}>
+            <View style={styles.boxStyle}>
+              <View style={styles.boxContent}>
+                <View style={styles.titleWrapperStyle}>
+                  <Text style={styles.componentTitleStyle}>Groups</Text>
+                </View>
+                <View style={styles.descWrapperStyle}>
+                  <Text>
+                    The
+                    <Text style={styles.descTextColor}>
+                      {' '}
+                      MeetupGroupListWithMessages
+                    </Text>{' '}
+                    component launches a Group list with messaging.
+                  </Text>
+                </View>
+                <View style={styles.linkContainer}>
+                  <TouchableOpacity
+                    style={styles.linkWrapperStyle}
+                    onPress={() => {
+                      props.navigation.navigate('Group');
+                    }}>
+                    <Text style={styles.linkStyle}>Launch</Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
+            </View>
+
+            <View style={styles.componentStyle}>
+              <View style={styles.boxStyle}>
+                <View style={styles.boxContent}>
+                  <View style={styles.titleWrapperStyle}>
+                    <Text style={styles.componentTitleStyle}>Users</Text>
+                  </View>
+                  <View style={styles.descWrapperStyle}>
+                    <Text>
+                      The{' '}
+                      <Text style={styles.descTextColor}>
+                        MeetupUserListWithMessages
+                      </Text>{' '}
+                      component launches a User list with messaging.
+                    </Text>
+                  </View>
+                  <View style={styles.linkContainer}>
+                    <TouchableOpacity
+                      style={styles.linkWrapperStyle}
+                      onPress={() => {
+                        props.navigation.navigate('Users');
+                      }}>
+                      <Text style={styles.linkStyle}>Launch</Text>
+                    </TouchableOpacity>
+                  </View>
+                </View>
+              </View>
+            </View>
+          </View>
+          <View style={styles.componentStyle}>
+            <View style={styles.boxStyle}>
+              <View style={styles.boxContent}>
+                <View style={styles.titleWrapperStyle}>
+                  <Text style={styles.componentTitleStyle}>
+                    Conversation List
+                  </Text>
+                </View>
+                <View style={styles.descWrapperStyle}>
+                  <Text>
+                    The{' '}
+                    <Text style={styles.descTextColor}>
+                      MeetupConversationList
+                    </Text>{' '}
+                    component launches Conversation list.
+                  </Text>
+                </View>
+                <View style={styles.linkContainer}>
+                  <TouchableOpacity
+                    style={styles.linkWrapperStyle}
+                    onPress={() => {
+                      props.navigation.navigate('ConversationComponent');
+                    }}>
+                    <Text style={styles.linkStyle}>Launch</Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
+            </View>
+          </View>
+          <View style={styles.componentStyle}>
+            <View style={styles.boxStyle}>
+              <View style={styles.boxContent}>
+                <View style={styles.titleWrapperStyle}>
+                  <Text style={styles.componentTitleStyle}>Group List</Text>
+                </View>
+                <View style={styles.descWrapperStyle}>
+                  <Text>
+                    The{' '}
+                    <Text style={styles.descTextColor}>MeetupGroupList</Text>{' '}
+                    component launches Group list.
+                  </Text>
+                </View>
+                <View style={styles.linkContainer}>
+                  <TouchableOpacity
+                    style={styles.linkWrapperStyle}
+                    onPress={() => {
+                      props.navigation.navigate('GroupComponent');
+                    }}>
+                    <Text style={styles.linkStyle}>Launch</Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
+            </View>
+          </View>
+          <View style={styles.componentStyle}>
+            <View style={styles.boxStyle}>
+              <View style={styles.boxContent}>
+                <View style={styles.titleWrapperStyle}>
+                  <Text style={styles.componentTitleStyle}>User List</Text>
+                </View>
+                <View style={styles.descWrapperStyle}>
+                  <Text>
+                    The{' '}
+                    <Text style={styles.descTextColor}>MeetupUserList</Text>{' '}
+                    component launches User list.
+                  </Text>
+                </View>
+                <View style={styles.linkContainer}>
+                  <TouchableOpacity
+                    style={styles.linkWrapperStyle}
+                    onPress={() => {
+                      props.navigation.navigate('UsersComponent');
+                    }}>
+                    <Text style={styles.linkStyle}>Launch</Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
+            </View>
+          </View>
+          <TouchableOpacity
+            style={styles.logoutBtn}
+            onPress={() => props.dispatch(actions.logout())}>
+            <Text style={styles.btnText}>Logout</Text>
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
+    </ScrollView>
+  );
+};
+
+const mapStateToProps = ({reducer}) => {
+  return {
+    loading: reducer.loading,
+    error: reducer.error,
+    isLoggedIn: reducer.isLoggedIn,
+  };
+};
+
+export default connect(mapStateToProps)(HomePage);
